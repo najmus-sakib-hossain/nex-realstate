41d6284799c8f74d928b2d0931becbfe394c9f27
najmus-sakib-hossain <manfromexistence@proton.me>
2025-12-08 00:44:20 +0600

    fix: Remove unnecessary max-width from Navbar container for improved responsiveness

--- resources/js/components/site-header.tsx
+++ resources/js/components/site-header.tsx
@@ -91,7 +91,7 @@ export function Navbar() {
             )} */}
 
             {/* Main Navigation */}
-            <div className="container mx-auto px-4 w-full lg:max-w-9xl">
+            <div className="container mx-auto w-full">
                 <div className="flex h-16 items-center justify-between">
                     {/* Logo */}
                     <Link href="/" className="flex items-center gap-2">
